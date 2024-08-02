19bcb2380659584c17dcb6c083bef0dc47da96c3
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-08-02 13:32:14 -0600

    function that loops through library array and displays each book on page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+const myLibrary = ["Example Book 1", "Example Book 2"];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -17,5 +17,17 @@ function Book(title, author, pages, read) {
 
 // TODO 1: make a function to add a book to the library
 function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
 
+// TODO 2: write a function that loops through array and displays each book on the page
+function displayBooks() {
+  const bookList = document.querySelector("#book-list");
+  bookList.textContent = "";
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    bookCard.innerHTML = "<p>" + book + "</p>";
+    bookList.appendChild(bookCard);
+  });
 }
