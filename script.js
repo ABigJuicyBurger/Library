ae28e3bc4f45e2e52ba1f9b67caf9cfd71b91986
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-08-05 13:20:19 -0600

    backup files added

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const myLibrary = ["Example Book 1", "Example Book 2"];
+const myLibrary = [];
+
 const addBookBtn = document.querySelector("#addBookBtn");
-const dialog = document.querySelector("#addBookDialog");
+const dialog = document.querySelector("#showBookDialog");
+const bookList = document.querySelector("#bookList");
+
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const bookPages = document.querySelector("#pages");
+const bookRead = document.querySelector("#read");
+
 const saveBook = document.querySelector("#saveBook");
 
 function Book(title, author, pages, read) {
@@ -15,27 +23,74 @@ function Book(title, author, pages, read) {
       return `${title} by ${author}, ${pages} pages, not read yet`;
     }
   };
+
+  this.display = function (container) {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    const titleP = document.createElement("p");
+    titleP.textContent = `Title: ${this.title}`;
+
+    const authorP = document.createElement("p");
+    authorP.textContent = `Author: ${this.author}`;
+
+    const pagesP = document.createElement("p");
+    pagesP.textContent = `Pages: ${this.pages}`;
+
+    const readP = document.createElement("p");
+    readP.textContent = `Read: ${this.read ? "Yes" : "No"}`;
+
+    bookCard.appendChild(titleP);
+    bookCard.appendChild(authorP);
+    bookCard.appendChild(pagesP);
+    bookCard.appendChild(readP);
+
+    container.appendChild(bookCard);
+  };
+
   console.log(this.info());
 }
 
-// TODO 1: make a function to add a book to the library
+Book.prototype.setIndex = function (index) {
+  this.bookCard.dataset.index = index;
+};
+
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-// TODO 2: write a function that loops through array and displays each book on the page
+addBookBtn.addEventListener("click", () => {
+  dialog.showModal();
+  console.log("add book button clicked");
+});
+
+saveBook.addEventListener("click", (e) => {
+  e.preventDefault();
+  const newBook = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    read.checked
+  );
+  addBookToLibrary(newBook);
+  displayBooks();
+  dialog.close();
+  resetDialogFields();
+});
+
 function displayBooks() {
-  const bookList = document.querySelector("#bookList");
   bookList.textContent = "";
-  myLibrary.forEach((book) => {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
-    bookCard.innerHTML = "<p>" + book + "</p>";
-    bookList.appendChild(bookCard);
+  myLibrary.forEach((book, index) => {
+    book.display(bookList);
+    book.setIndex(index);
   });
 }
 
-// TODO 3: Use the addBook button in HTML to bring up a form allowing user to input details of book: title, author, pages and whether it's been read using dialog box
-addBookBtn.addEventListener("click", () => {
-  dialog.showModal();
-});
+function resetDialogFields() {
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  read.checked = false;
+}
+
+displayBooks();
